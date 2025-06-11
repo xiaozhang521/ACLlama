96b9a2f69be79f9b9ac82db46ab0a4e8a5c7055d
shanweiqiao <1024476687@qq.com>
2025-06-11 14:44:02 +0800

    add npu training and inference scripts

---
+++ run_npu.sh
@@ -0,0 +1,88 @@
+#! /bin/bash
+
+device=(0,1,2,3,4,5,6,7)
+gpu_num=8
+# device=(0)
+# gpu_num=1
+
+
+export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=1
+export TORCH_COMPILE=0
+export DISABLE_TORCH_COMPILE=1
+export NCCL_DEBUG=INFO
+export TORCH_DISTRIBUTED_DEBUG=DETAIL
+
+output_tag="../ACLlama_output/whisper-large-v3-and-Llama-3.2-3B-align"
+
+export ASCEND_VISIBLE_DEVICES=${device}
+export ASCEND_DEVICE_ID=${device}
+cmd="torchrun
+    --nproc_per_node 8
+    --nnodes 1
+    --node_rank 0
+    --master_addr localhost
+    --master_port 6601
+    finetune_acllama_npu.py
+    --audio_model_name_or_path "/data/s50042884/huggingface_model/whisper-large-v3"
+    --text_model_name_or_path "/data/s50042884/my_code/ACLlama_output/ACLlama_finetune_whisper"
+    --data_path "/data/s50042884/huggingface_model/libri_train_update.json"
+    --output_dir ${output_tag}
+    --num_train_epochs 40
+    --fp16 True
+    --per_device_train_batch_size 4
+    --per_device_eval_batch_size 1
+    --gradient_accumulation_steps 64
+    --evaluation_strategy "no"
+    --save_strategy "steps"
+    --save_steps 100
+    --save_total_limit 100
+    --learning_rate 1e-4
+    --weight_decay 0.1
+    --adam_beta2 0.95
+    --warmup_ratio 0.01
+    --lr_scheduler_type "cosine"
+    --logging_steps 1
+    --report_to "none"
+    --model_max_length 512
+    --gradient_checkpointing True
+    --deepspeed "./config/ds_config_zero2.json"
+    --use_lora"
+
+save_cmd="${output_tag}/train.log"
+echo $cmd
+eval $cmd 2>&1 | tee $save_cmd
+
+
+    # --per_device_train_batch_size 32 \
+    # --per_device_eval_batch_size 1 \
+    # --gradient_accumulation_steps 8 \
+    # --deepspeed "./config/ds_config_zero2.json" \
+    # --fp16 True \
+    # --bf16 True \
+
+# python3 finetune_acllama.py \
+#     --audio_model_name_or_path "/data/s50042884/huggingface_model/whisper-large-v3" \
+#     --text_model_name_or_path "/data/s50042884/my_code/ACLlama_output/ACLlama_lora" \
+#     --data_path "/data/s50042884/huggingface_model/libri_train_update.json" \
+#     --fp16 True \
+#     --output_dir "../ACLlama_output/ACLlama_lora" \
+#     --num_train_epochs 40 \
+#     --per_device_train_batch_size 4 \
+#     --per_device_eval_batch_size 1 \
+#     --gradient_accumulation_steps 64 \
+#     --evaluation_strategy "no" \
+#     --save_strategy "steps" \
+#     --save_steps 100 \
+#     --save_total_limit 1 \
+#     --learning_rate 1e-5 \
+#     --weight_decay 0.1 \
+#     --adam_beta2 0.95 \
+#     --warmup_ratio 0.01 \
+#     --lr_scheduler_type "cosine" \
+#     --logging_steps 1 \
+#     --report_to "none" \
+#     --model_max_length 512 \
+#     --gradient_checkpointing True \
+#     --deepspeed "./config/ds_config_zero2.json" \
+#     --use_lora
